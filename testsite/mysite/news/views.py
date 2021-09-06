dad651ab49e72e21d9aeb9c28e299c222b70f2d0
KhalitovRA <halitov-02@mail.ru>
2021-09-06 12:51:52 +0300

    commit 12 video admin

--- testsite/mysite/news/views.py
+++ testsite/mysite/news/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import News
 
 def index(request):
-    #print(request)
-    return HttpResponse('Hello world')
+    news = News.objects.all()
+    context = {
+        'news': news,
+        'title': 'Список новостей'
+    }
+    return render(request, template_name='news/index.html', context=context)
 
-def test(request):
-    return HttpResponse('<h1>Test page <h1>')
