316c42be9915db4e3966d19e1860f851b4dd17ad
KhalitovRA <halitov-02@mail.ru>
2021-09-15 20:35:01 +0300

    commit 23 video (forms)

--- testsite/mysite/news/views.py
+++ testsite/mysite/news/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 
 from .models import News, Category
-
+from .forms import NewsForms
 
 def index(request):
     news = News.objects.all()
@@ -17,8 +17,16 @@ def get_category(request, category_id):
     category = Category.objects.get(pk=category_id)
     return render(request, 'news/category.html', {'news': news, 'category': category})
 
+
 def view_news(request, news_id):
     #news_item = News.objects.get(pk=news_id)
     news_item = get_object_or_404(News, pk=news_id)
     return render(request, 'news/view_news.html', {'news_item': news_item})
 
+
+def add_news(request):
+    if request.method ==  'POST':
+        pass
+    else:
+        form = NewsForms()
+    return render(request, 'news/add_news.html', {'form': form})
